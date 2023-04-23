ab4c57681d16b9e6a4c7970b4b043b3e69a51276
Gustavo Mendonça <alf4r6@gmaim.com>
2023-04-23 12:43:05 -0300

    fix: typing loop

--- src/components/Typing.tsx
+++ src/components/Typing.tsx
@@ -1,15 +1,16 @@
-"use client"
+'use client';
 
 import Typewriter from 'typewriter-effect';
 export function Typing() {
   return (
     <Typewriter
-      options={{
-        strings: ["Gustavo Mendonça"],
-        autoStart: true,
-        delay: 100,
-        loop: true,
-        pauseFor: 10000
+      onInit={(typewriter) => {
+        typewriter
+          .typeString('Gustavo Mendonça')
+          .callFunction(() => {
+            console.log('String typed out!');
+          })
+          .start();
       }}
     />
   );

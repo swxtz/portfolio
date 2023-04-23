806454330b1e1e6e0ea772b36975496fa2a85926
Gustavo Mendonça <alf4r6@gmaim.com>
2023-04-23 15:59:25 -0300

    feat: island image responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Greetings } from '@/components/Greetings';
 import { Header } from '@/components/Header';
+import islandImg from "../../public/island-with-computer.png"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,6 +13,9 @@ export default function Home() {
       <main>
         <div className="grid grid-cols-2">
           <Greetings />
+          <div className="flex items-center justify-center">
+            <Image src={islandImg} alt="" priority width={600}/>
+          </div>
         </div>
       </main>
     </div>

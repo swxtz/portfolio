9c55f42f4c39a9a3dfea8246721405bbfcbfafb0
Gustavo Mendonça <alf4r6@gmaim.com>
2023-04-23 18:02:33 -0300

    feat: island image responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,17 @@ import './globals.css';
 
 import { Fira_Code } from 'next/font/google';
 import React from 'react';
+import { Header } from '@/components/Header';
 
 const firaCode = Fira_Code({
   subsets: ['latin'],
-  display: "swap",
-  preload: true
+  display: 'swap',
+  preload: true,
 });
-
 
 export const metadata = {
   title: 'Gustavo Mendonca | Portfolio',
   description: 'Hi, Im Full-Stack Developer from Brazil',
-  
 };
 
 export default function RootLayout({
@@ -25,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={clsx('bg-background text-gray-500', firaCode.className)}>
+      <body className={clsx('bg-background text-gray-500 h-screen w-screen px-24 overflow-hidden', firaCode.className)}>
+        <nav>
+          <Header title="<GustavoMendonça />" />
+        </nav>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen px-24">
-      <nav>
-        <Header title="<GustavoMendonça />" />
-      </nav>
+    <div className="h-screen w-screen">
 
       <main>
         <div className="grid grid-cols-2">
           <Greetings />
           <div className="flex items-center justify-center">
-            <Image src={islandImg} alt="" priority width={600}/>
+            <Image src={islandImg} alt="" priority width={600} className="pr-24"/>
           </div>
         </div>
       </main>

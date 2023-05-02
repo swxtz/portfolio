bcbadab715891c733a178eaf8f13183e773358f0
Gustavo Mendonça <alf4r6@gmaim.com>
2023-05-02 19:24:24 -0300

    feat: change tag a for next/link

--- src/components/HeaderButtons.tsx
+++ src/components/HeaderButtons.tsx
@@ -1,17 +1,20 @@
+
+import Link from "next/link";
+ 
 export function HeaderButtons() {
   return (
     <div className="flex flex-row items-center">
       <div className="px-9 border-r border-gray-500">
-        <a href="/" className="hover:text-gray-300/80 transition-colors">_Hello</a>
+        <Link href="/" className="hover:text-gray-300/80 transition-colors">_Hello</Link>
       </div>
       <div className="px-9 border-r border-gray-500">
-        <a href="/about" className="hover:text-gray-300/80 transition-colors">_About</a>
+        <Link href="/about" className="hover:text-gray-300/80 transition-colors">_About</Link>
       </div>
       <div className="px-9 border-r border-gray-500">
-        <a href="/projects" className="hover:text-gray-300/80 transition-colors">_Projects</a>
+        <Link href="/projects" className="hover:text-gray-300/80 transition-colors">_Projects</Link>
       </div>
       <div className="px-9">
-        <a href="#" className="hover:text-gray-300/80 transition-colors">_Contact-me</a>
+        <Link href="/contact" className="hover:text-gray-300/80 transition-colors">_Contact-me</Link>
       </div>
     </div>
   );

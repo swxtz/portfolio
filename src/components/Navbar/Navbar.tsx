8d63ad4c1f14f6fb3c42bda6fcdf06fc49a73318
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-14 17:16:18 -0300

    Update Navbar border color

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLinks } from "../NavLinks/NavLinks";
 
 export function Navbar() {
     return (
-        <nav className="bg-[#2B2B2B]">
+        <nav className="border-b-2 border-[#444444]">
             <div className="py-6 px-8 shadow-sm flex flex-row justify-between">
                 <Link href="/"><span className="text-2xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span></Link>
                 <NavLinks />

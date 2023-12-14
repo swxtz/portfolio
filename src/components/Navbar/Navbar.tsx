50bd9e3d7982a11943e7890c946068359b067958
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-14 01:04:56 -0300

    feat: add link to home page in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import { NavLinks } from "../NavLinks/NavLinks";
 
 export function Navbar() {
     return (
         <nav className="bg-[#2B2B2B]">
             <div className="py-10 px-12 shadow-sm flex flex-row justify-between">
-                <span className="text-3xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span>
+                <Link href="/"><span className="text-3xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span></Link>
                 <NavLinks />
             </div>
         </nav>

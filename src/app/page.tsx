a4a4062e2853c65b129dd785af180ec8c2f535be
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-14 01:41:25 -0300

    Add Socials component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/Hero/Hero";
+import { Socials } from "@/components/Socials/Socials";
 
 export default function Home() {
     return (
         <main>
             <div className="px-24 pt-[100px]">
                 <Hero />
+                <Socials />
             </div>
         </main>
     );
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -6,7 +6,7 @@ import emailLogo from "@/assets/svg/email.svg";
 
 export function Socials() {
     return (
-        <div className="">
+        <div className="flex gap-4 mt-12">
             <SocialLinks href="https://github.com/swxtz" img={githubLogo} alt="github" />
             <SocialLinks href="" img={linkedinLogo} alt="linkedin" />
             <SocialLinks href="" img={emailLogo} alt="email" /> 

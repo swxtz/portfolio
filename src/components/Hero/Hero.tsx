f1f5524ba67140860498bc9e4e903a9855622e0d
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-14 16:06:38 -0300

    fix: hero line  height

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,10 +7,9 @@ export function Hero() {
             <p className="text-5xl tracking-[.23rem] mt-[18px]">
                 <span className="text-teal-500">Full-Stack </span>
                 Developer
-                <div className="py-3"></div>
-                &&
                 <br />
-                <div className="py-3"></div>
+                <span className="leading-relaxed">&&</span>
+                <br />
                 <span className="text-teal-500">Estudante </span>
                 de Programação
             </p>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavLinks } from "../NavLinks/NavLinks";
 export function Navbar() {
     return (
         <nav className="bg-[#2B2B2B]">
-            <div className="py-10 px-12 shadow-sm flex flex-row justify-between">
+            <div className="py-10 px-8 shadow-sm flex flex-row justify-between">
                 <Link href="/"><span className="text-3xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span></Link>
                 <NavLinks />
             </div>

b1e72d8e6a7829d63ef3383dd625525dfeac0405
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-14 16:11:13 -0300

    style: reduce navbar font size

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import { NavLinks } from "../NavLinks/NavLinks";
 export function Navbar() {
     return (
         <nav className="bg-[#2B2B2B]">
-            <div className="py-10 px-8 shadow-sm flex flex-row justify-between">
-                <Link href="/"><span className="text-3xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span></Link>
+            <div className="py-6 px-8 shadow-sm flex flex-row justify-between">
+                <Link href="/"><span className="text-2xl tracking-[.14rem]">{"<Gustavo Mendonça />"}</span></Link>
                 <NavLinks />
             </div>
         </nav>
